08600594da15e47faeb718ae40b05be064d41484
Joaquim José Almeida Pereira <joaquim.org@gmail.com>
2021-04-13 13:44:48 +0100

    pwa support

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ Vue.config.productionTip = false
 //https://github.com/ankurk91/vue-toast-notification
 import VueToast from 'vue-toast-notification'
 import 'vue-toast-notification/dist/theme-sugar.css'
+import './registerServiceWorker'
 
 Vue.use(VueToast, {
   // One of the options
